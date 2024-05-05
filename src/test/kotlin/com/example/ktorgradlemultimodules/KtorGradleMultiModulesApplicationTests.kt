144250e5bbd3afcd268c9bdfb5afca8fd58736ba
mohadang <ehdlaos@gmail.com>
2024-05-05 13:20:25 +0900

    init

---
+++ src/test/kotlin/com/example/ktorgradlemultimodules/KtorGradleMultiModulesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ktorgradlemultimodules
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtorGradleMultiModulesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
